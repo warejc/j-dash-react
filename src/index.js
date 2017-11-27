46003bff4c65d2273feefc215565a78191ee27de
Julius Ware <jcware8@gmail.com>
2017-11-27 14:33:55 -0500

    First Commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reducers from './reducers';
+import { Col, Grid, Panel, Row} from 'react-bootstrap';
+import { createStore, applyMiddleware } from 'redux';
+import { HashRouter as Router, Link, Redirect, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+
+
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+
+
+
+
+    document.getElementById('root')
+
+
+);
